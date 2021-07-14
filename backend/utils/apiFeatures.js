7adca606a2bfc6dc3b3195a931308d5d4316b0ad
DabaniCode <dabani.rm@gmail.com>
2021-07-14 18:29:33 +0200

    Filter Products

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -15,6 +15,21 @@ class APIFeatures {
     this.query = this.query.find({...keyword});
     return this;
   }
+
+  filter() {
+    const queryCopy = { ...this.queryStr };
+
+    // Removing fields from the query
+    const removeFields = ['keyword', 'limit', 'page'];
+    removeFields.forEach(el => delete queryCopy[el]);
+
+    // Advance filter for price, ratings, etc
+    let queryStr = JSON.stringify(queryCopy);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`); /* We add double dollar to add an $ to mongodb operators */
+
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
 }
 
 module.exports = APIFeatures;
