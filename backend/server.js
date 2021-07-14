544e2511325af6c90810317323dfed5cc8414cfd
DabaniCode <dabani.rm@gmail.com>
2021-07-14 13:39:02 +0200

    Handling Uncaught Exceptions

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,13 @@ const connectDatabase = require('./config/database');
 
 const dotenv = require('dotenv');
 
+// Handle Uncaught exception
+process.on('uncaughtException', err => {
+  console.log(`ERROR: ${err.message}`);
+  console.log(`Shutting down due to uncaught exceptions.`);
+  process.exit(1);
+});
+
 // Setting up config file
 dotenv.config({ path: 'backend/config/config.env'});
 
@@ -16,8 +23,8 @@ const server = app.listen(process.env.PORT, () => {
 // Handle Unhandled Promise Rejections
 process.on('unhandledRejection', err => {
   console.log(`ERROR: ${err.message}`);
-  console.log(`Shutting down the server due to Unhandled Promise rejection`);
+  console.log(`Shutting down due to Unhandled Promise rejection`);
   server.close(() => {
-    process.exit(l);
+    process.exit(1);
   });
 });
