252dc508d283b7a32a0dcecc49a779331576f794
DabaniCode <dabani.rm@gmail.com>
2021-07-14 12:10:33 +0200

    Creating Errors Middleware

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,7 @@
 const Product = require('../models/product');
 
+const ErrorHandler = require('../utils/errorHandler');
+
 // Create new product => /api/v1/admin/product/new
 exports.newProduct = async (req, res, next) => {
 
@@ -29,10 +31,7 @@ exports.getSingleProduct = async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
   if (!product) {
-    return res.status(404).json({
-      success: false,
-      message: 'Product not found'
-    });
+    return next(new ErrorHandler('Product not found', 404));
   }
   
   res.status(200).json({
@@ -47,10 +46,7 @@ exports.updateProduct = async (req, res, next) => {
   let product = await Product.findById(req.params.id);
 
   if (!product) {
-    return res.status(404).json({
-      success: false,
-      message: 'Product not found'
-    })
+    return next(new ErrorHandler('Product not found', 404));
   }
 
   product = await Product.findByIdAndUpdate(req.params.id, req.body, {
@@ -71,10 +67,7 @@ exports.deleteProduct = async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
   if (!product) {
-    return res.status(404).json({
-      success: false,
-      message: 'Product not found.'
-    });
+    return next(new ErrorHandler('Product not found', 404));
   }
 
   await product.remove();
