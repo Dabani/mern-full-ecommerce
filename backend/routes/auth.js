e9d29152a7827bc41e99fae1359e819973bd1095
DabaniCode <dabani.rm@gmail.com>
2021-07-16 09:52:29 +0200

    Logout User

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,10 +3,13 @@ const router = express.Router();
 
 const { 
   registerUser,
-  loginUser
+  loginUser,
+  logout
 } = require('../controllers/authController');
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
+
+router.route('/logout').get(logout);
 
 module.exports = router;
