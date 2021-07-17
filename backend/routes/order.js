1f151a780ab3b6a2fe7eecb10159a166769a0924
DabaniCode <dabani.rm@gmail.com>
2021-07-17 10:46:13 +0200

    Get Single Order and Logged in User Orders

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const {
-  newOrder
+  newOrder,
+  getSingleOrder,
+  myOrders
 } = require('../controllers/orderController');
 
 const {
@@ -11,5 +13,8 @@ const {
 } = require('../middlewares/auth');
 
 router.route('/order/new').post(isAuthenticatedUser, newOrder);
+
+router.route('/order/:id').get(isAuthenticatedUser, getSingleOrder);
+router.route('/orders/me').get(isAuthenticatedUser, myOrders);
 
 module.exports = router;
