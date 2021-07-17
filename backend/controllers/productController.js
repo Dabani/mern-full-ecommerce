b58ce4ccdb4c99a0068f9465a8f667f873a7ada8
DabaniCode <dabani.rm@gmail.com>
2021-07-17 12:33:32 +0200

    Add New & Update Review

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -90,3 +90,47 @@ exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
     message: 'Product deleted successfully.'
   });
 })
+
+// Create new review => /api/v1/review
+exports.createProductReview = catchAsyncErrors(async(req, res, next) => {
+
+  const {
+    rating,
+    comment,
+    productId
+  } = req.body;
+
+  const review = {
+    user: req.user._id,
+    name: req.user.name,
+    rating: Number(rating),
+    comment
+  }
+
+  const product = await Product.findById(productId);
+
+  const isReviewed = product.reviews.find(
+    r => r.user.toString() === req.user._id.toString() /* r for review */
+  )
+  
+  if (isReviewed) {
+    product.reviews.forEach(review => {
+      if (review.user.toString() === req.user._id.toString()) {
+        review.comment = comment;
+        review.rating = rating;
+      }
+    })
+  } else {
+    product.reviews.push(review);
+    product.numOfReviews = product.review.length;
+  }
+
+  // Calculate overall rating
+  product.ratings = product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.reviews.length;
+
+  await product.save({ validateBeforeSave: false });
+
+  res.status(201).json({
+    success: true
+  });
+});
